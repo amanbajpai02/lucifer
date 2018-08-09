69883bbf62ab50a0952c5bde4d520bfbb3af2987
amanbajpai02 <42239254+amanbajpai02@users.noreply.github.com>
2018-08-09 16:50:00 +0530

    Add files via upload
    These are the Lab2 files..

---
+++ LAB2_2.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   char str[100];
+   int length;
+   printf("\nEnter the String : ");
+   gets(str);
+   length = 0;
+   for(length=0;str[length]!='\0';length++);
+   printf("\nLength of the String is : %d", length);
+   return(0);
+}
---
+++ LAB2_3.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void minmax(int a[],int len,int *min,int *max)
+{
+int i;
+*max = a[0];
+*min = a[0];
+for(i = 0 ; i < len ; i++)
+{
+if(*max < a[i])
+*max = a[i];
+if(*min > a[i])
+*min = a[i];
+}
+}
+int main()
+{
+int a[100],i,len,min,max;
+printf("\nPlease enter the length of array:");
+scanf("%d",&len);
+for(i = 0 ; i < len ; i++)
+{
+printf("Enter the a[%d] values: ",i);
+scanf("%d",&a[i]);
+}
+minmax(a,len,&min,&max);
+printf("\nMaximum values is: %d \nMinimum value is: %d",max,min);
+getch();
+return 0;
+}
